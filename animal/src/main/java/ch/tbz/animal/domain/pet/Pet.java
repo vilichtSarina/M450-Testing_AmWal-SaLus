666570ee4624990df6f7cf68d4c360010d584706
vilichtSarina <sarina.lusti@noseryoung.ch>
2023-08-22 11:16:31 +0200

    Add food property to pet entity.

--- animal/src/main/java/ch/tbz/animal/domain/pet/Pet.java
+++ animal/src/main/java/ch/tbz/animal/domain/pet/Pet.java
@@ -1,6 +1,7 @@
 package ch.tbz.animal.domain.pet;
 
 import ch.tbz.animal.core.generic.AbstractEntity;
+import ch.tbz.animal.domain.stat.Food;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinTable;
@@ -8,19 +9,25 @@ import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import org.antlr.v4.runtime.misc.NotNull;
 
+import java.util.UUID;
+
 @Entity
 public class Pet extends AbstractEntity {
 
     @Column(name = "name")
     private String name;
 
+    @JoinTable(name = "food_stat")
+    @OneToOne
+    private Food food;
+
+    public Pet(UUID id, String name, Food food) {
+        super(id);
+        this.name = name;
+        this.food = food;
+    }
 
     public Pet() {
 
     }
-
-//    @JoinTable(name = "owner")
-//    @OneToOne
-//    @NotNull
-//    private User owner;
 }
