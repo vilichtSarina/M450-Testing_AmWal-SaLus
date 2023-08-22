a6d813e727bd10b5d878e7f6feb71d4ee00d702c
AW0000 <amanda.walser@noseryoung.ch>
2023-08-22 11:23:42 +0200

    Add the Love stat to the pet

--- animal/src/main/java/ch/tbz/animal/domain/pet/Pet.java
+++ animal/src/main/java/ch/tbz/animal/domain/pet/Pet.java
@@ -2,6 +2,7 @@ package ch.tbz.animal.domain.pet;
 
 import ch.tbz.animal.core.generic.AbstractEntity;
 import ch.tbz.animal.domain.stat.Food;
+import ch.tbz.animal.domain.stat.Love;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinTable;
@@ -19,10 +20,15 @@ public class Pet extends AbstractEntity {
     @OneToOne
     private Food food;
 
-    public Pet(UUID id, String name, Food food) {
+    @JoinTable(name = "love_time")
+    @OneToOne
+    private Love love;
+
+    public Pet(UUID id, String name, Food food, Love love) {
         super(id);
         this.name = name;
         this.food = food;
+        this.love = love;
     }
 
     public Pet() {
