3719ad6dc1f642417509f57186cfd7cd6359fcc3
vilichtSarina <sarina.lusti@noseryoung.ch>
2023-08-22 10:52:19 +0200

    create initial project

---
+++ animal/src/test/java/ch/tbz/animal/AnimalApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.tbz.animal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
