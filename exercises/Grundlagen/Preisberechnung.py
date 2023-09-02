171a15651c6f1034b671b9c6231647f8fbfc4551
AW0000 <amanda.walser@noseryoung.ch>
2023-09-02 13:20:50 +0200

    Finished the Grundlagen exercise

--- exercises/Grundlagen/Preisberechnung.py
+++ exercises/Grundlagen/Preisberechnung.py
@@ -1,17 +1,18 @@
 import math
+import unittest
 
 def calculatePrice(baseprice, specialprice, extraprice, extras, discount):
     addon_discount = 0
-    result = 0
+    #result = 0 -> diese Zeile braucht es nicht!
 
-    if extras >= 3:
+    if ((extras >= 3) and (extras < 5)):
         addon_discount = 10
     else:
         if extras >= 5:
             addon_discount = 15
 
-    if discount > addon_discount:
-        addon_discount = discount
+    #if discount > addon_discount: #This is wrong, there is no relation between addon discount and discount!
+       # addon_discount = discount
 
     result = baseprice * (1 - discount / 100.0) + specialprice + extraprice * (1 - addon_discount / 100.0)
 
@@ -21,7 +22,7 @@ def calculatePrice(baseprice, specialprice, extraprice, extras, discount):
 def test_calculate_price():
     test_ok = True
 
-    # Test case 1: extras < 3, discount < addon_discount
+    # Test case 1: extras < 3
     price = calculatePrice(100.0, 50.0, 20.0, 2, 5.0)
     if not math.isclose(price, 165.0, rel_tol=1e-6):
         print("Test case 1 failed. Expected: 165.0, Actual:", price)
@@ -29,23 +30,21 @@ def test_calculate_price():
     else:
         print("Test case 1 passed.")
 
-    # Test case 2: extras >= 3, discount < addon_discount
+    # Test case 3: extras >= 3
     price = calculatePrice(200.0, 75.0, 30.0, 4, 8.0)
-    if not math.isclose(price, 275.0, rel_tol=1e-6):
-        print("Test case 2 failed. Expected: 275.0, Actual:", price)
+    if not math.isclose(price, 286.0, rel_tol=1e-6):
+        print("Test case 2 failed Expected: 286.0, Actual:", price)
         test_ok = False
     else:
         print("Test case 2 passed.")
 
-    # Test case 3: extras >= 5, discount > addon_discount
-    price = calculatePrice(150.0, 60.0, 25.0, 6, 20.0)
-    if not math.isclose(price, 192.5, rel_tol=1e-6):
-        print("Test case 3 failed. Expected: 192.5, Actual:", price)
+    # Test case 3: extras >= 5
+    price = calculatePrice(200.0, 75.0, 30.0, 6, 8.0)
+    if not math.isclose(price, 284.5, rel_tol=1e-6):
+        print("Test case 3 failed Expected: 284.5, Actual:", price)
         test_ok = False
     else:
         print("Test case 3 passed.")
-
-    # Add more test cases if needed
 
     if test_ok:
         print("All test cases passed!")
@@ -54,8 +53,8 @@ def test_calculate_price():
 
 
 # Call calculatePrice with specific arguments
-result = calculatePrice(250.0, 80.0, 40.0, 7, 12.0)
+result = calculatePrice(100.0, 50.0, 20.0, 2, 5.0)
 print("Result:", result)
 
-# Call the test_calculate_price method
+#Call the test_calculate_price method
 test_calculate_price()
