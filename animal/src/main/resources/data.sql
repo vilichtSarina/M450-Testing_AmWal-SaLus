8707f1c3b3c3a3e96b3956d973adedcd5fab3f1a
vilichtSarina <sarina.lusti@noseryoung.ch>
2023-09-26 10:22:44 +0200

    rewrote application.properties to connect with DB

--- animal/src/main/resources/data.sql
+++ animal/src/main/resources/data.sql
@@ -1,5 +1,17 @@
 --ROLES
-INSERT INTO role(id, name)
-VALUES ('d29e709c-0ff1-4f4c-a7ef-09f656c390f1', 'DEFAULT'),
-       ('ab505c92-7280-49fd-a7de-258e618df074', 'PRIVILEGED') ON CONFLICT DO NOTHING;
+-- INSERT INTO role(id, name)
+-- VALUES ('d29e709c-0ff1-4f4c-a7ef-09f656c390f1', 'DEFAULT'),
+--        ('ab505c92-7280-49fd-a7de-258e618df074', 'PRIVILEGED') ON CONFLICT DO NOTHING;
 
+--AUTHORITIES
+INSERT INTO authority(id, name)
+VALUES ('2ebf301e-6c61-4076-98e3-2a38b31daf86', 'DEFAULT'),
+       ('76d2cbf6-5845-470e-ad5f-2edb9e09a868', 'PRIVILEGED')
+    ON CONFLICT DO NOTHING;
+
+-- --assign authorities to roles
+-- INSERT INTO role_authority(role_id, authority_id)
+-- VALUES ('d29e709c-0ff1-4f4c-a7ef-09f656c390f1', '2ebf301e-6c61-4076-98e3-2a38b31daf86'),
+--        ('ab505c92-7280-49fd-a7de-258e618df074', '2ebf301e-6c61-4076-98e3-2a38b31daf86'),
+--        ('ab505c92-7280-49fd-a7de-258e618df074', '76d2cbf6-5845-470e-ad5f-2edb9e09a868')
+--     ON CONFLICT DO NOTHING;
