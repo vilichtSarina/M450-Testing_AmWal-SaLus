2cfa81a484912b44b2624b1f87987f261916ba0a
vilichtSarina <sarina.lusti@noseryoung.ch>
2023-10-30 19:05:19 +0100

    Add StringStack and DoubleStack.

---
+++ project/PolyPlotter/src/test/java/ch/tbz/graph/interpreter/StringStackTest.java
@@ -0,0 +1,59 @@
+package ch.tbz.graph.interpreter;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StringStackTest {
+
+    private StringStack stack;
+
+    public StringStackTest() {
+        this.stack = new StringStack();
+    }
+
+    @BeforeEach
+    public void resetStack() {
+        stack = new StringStack();
+    }
+
+    @Test
+    public void addsOnPush_WithValidNewValue() {
+        String firstValue = "first";
+        String secondValue = "second";
+
+        stack.push(firstValue);
+        stack.push(secondValue);
+
+        assertEquals(stack.pop(), secondValue);
+        assertEquals(stack.pop(), firstValue);
+    }
+
+    @Test
+    public void testPop_WithNonNullStack() {
+        String value = "someValue";
+        stack.push(value);
+
+        assertEquals(stack.pop(), value);
+        assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    public void testIsEmpty_WithEmptyStack() {
+        assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    public void testIsFull_WithNonNullStack() {
+        String value = "someValue";
+        int maximumIndex = 9;
+        for (int i= 0; i < maximumIndex; i++) {
+            stack.push(value);
+        }
+        assertTrue(stack.isFull());
+    }
+}
