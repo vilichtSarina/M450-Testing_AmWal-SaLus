c01db81f99849889bf70ad742c7b1bf2b01076e3
vilichtSarina <sarina.lusti@noseryoung.ch>
2023-10-30 21:39:17 +0100

    Add comment to FunctionFormatException

--- project/PolyPlotter/src/main/java/ch/tbz/exception/FunctionFormatException.java
+++ project/PolyPlotter/src/main/java/ch/tbz/exception/FunctionFormatException.java
@@ -1,5 +1,11 @@
 package ch.tbz.exception;
 
+/**
+ * This exception occurs during interpreting.
+ * It's extended by the more specific ParenthesesMismatchException.
+ * 
+ * As a general idea, this class has the purpose of surfacing the exception whenever a given input in the
+ * GraphInputMenu cannot be converted into a valid mathematical function and or graph.*/
 public class FunctionFormatException extends NumberFormatException{
     public FunctionFormatException(String functionInput) {
         super("The input \"" + functionInput + "\" doesn't follow the requirements.");
